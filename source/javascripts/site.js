06b7688f5b42fbf416f0cf2f1c82d190128724d6
laure peucelle <laurepeu@hotmail.com>
2018-05-23 17:24:08 +0200

    added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#big-logo').hide().fadeIn(3000);
+    $('#big-logo').hide().fadeIn(10000);
 });
 
 var showText = function (target, message, index, interval) {    
@@ -9,6 +9,7 @@ var showText = function (target, message, index, interval) {
   } 
 }
     
+
 $(function () { 
  
   showText("#msg", "Make My Day accompagne les PME, les TPE, les indépendants et les associations dans la promotion de leur image, l’élaboration d’une stratégie médiatique et la mise en place de relations presse.", 0, 60);    
